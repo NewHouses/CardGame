b735952eb3adcef13684f441e0d8ac1fbf30a52f
NewHouses <sergio.simons.c@gmail.com>
2023-09-20 19:10:22 +0200

    Functions and return types

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	card := "Ace of Spades"
+	card = newCard()
+
+	fmt.Println(card)
+	printState()
+}
+
+func newCard() string {
+	return "Five of Diamonds"
+}
