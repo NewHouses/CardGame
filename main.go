023bc19f08e939e1466992997cc5398a53000034
NewHouses <sergio.simons.c@gmail.com>
2023-10-05 19:43:36 +0200

    Saving deck into a text file

--- main.go
+++ main.go
@@ -1,14 +1,21 @@
 package main
+
 import "fmt"
 
 func main() {
 	cards := newDeck()
+	cards.saveToFile("my_cards")
+
 	hand, remainingDeck := deal(cards, 5)
 
 	fmt.Println("My hand")
 	hand.print()
 	fmt.Println("\n\nRemaining deck")
 	remainingDeck.print()
+
+	hand.toString()
+	fmt.Println(hand.toString())
+
 }
 
 func newCard() string {
