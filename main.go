f3e9239249d27881529f91e473a74227836c1e4b
NewHouses <sergio.simons.c@gmail.com>
2023-10-05 20:14:10 +0200

    Creating deck from stored file

--- main.go
+++ main.go
@@ -1,10 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	cards := newDeck()
-	cards.saveToFile("my_cards")
+	var answer string
+	var cards deck
+	fmt.Println("Do you want to create a new Deck? (y/n)")
+	fmt.Scanln(&answer)
+
+	if answer == "y" {
+		cards = newDeck()
+		cards.saveToFile("my_cards")
+	} else if answer == "n" {
+		var filename string
+		fmt.Println("Introduce the deck file path?")
+		fmt.Scanln(&filename)
+		cards = newDeckFromFile(filename)
+	} else {
+		fmt.Println("Not valid answer")
+		os.Exit(1)
+	}
 
 	hand, remainingDeck := deal(cards, 5)
 
